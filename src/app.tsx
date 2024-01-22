6ae4b91289a1dbe1dd880d8ded3ecc1dbd05f5d2
Chengjie Mao <minnows.lodge_0s@icloud.com>
2024-01-21 22:24:33 -0500

    Use react to render html page

---
+++ src/app.tsx
@@ -0,0 +1,3 @@
+export function App() {
+	return <h1>Best Before</h1>
+}
--- src/client.tsx
+++ src/client.tsx
@@ -1,1 +1,5 @@
-console.log("hello")
+import { App } from './app'
+import { createRoot } from 'react-dom/client'
+
+const root = createRoot(document.getElementById("app")!)
+root.render(<App />)
