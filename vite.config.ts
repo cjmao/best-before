017beb9b220dd31df343311cc04ad7439a81107c
Chengjie Mao <minnows.lodge_0s@icloud.com>
2024-01-22 23:55:37 -0500

    Update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,17 @@
 import { defineConfig } from 'vite'
 import pages from '@hono/vite-cloudflare-pages'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig(({ mode }) => {
-	return mode !== 'client' ? {
+	return mode === 'server' ? {
 		plugins: [pages()]
 	} : {
 		build: {
 			emptyOutDir: false,
-			minify: true
+			minify: mode !== 'development',
+			sourcemap: mode === 'development',
+			watch: mode === 'development' ? {} : null
 		},
+		plugins: [react()]
 	}
 })
