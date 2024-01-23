06e9bdb1b59859a24637c494ac44372d168f4a6a
Chengjie Mao <minnows.lodge_0s@icloud.com>
2024-01-23 01:44:28 -0500

    Add code to fetch items from server

--- src/app.tsx
+++ src/app.tsx
@@ -1,3 +1,36 @@
+import './app.css'
+import { AppType } from './index'
+import { hc } from 'hono/client'
+import { Inventory } from './components/inventory'
+import { Item } from './schema'
+import { useEffect, useState } from 'react'
+
+const client = hc<AppType>("/")
+
 export function App() {
-	return <h1>Best Before</h1>
+	const [items, setItems] = useState([] as Item[])
+
+	useEffect(() => {
+		let ignore = false
+
+		async function getItems() {
+			const response = await client.items.$get()
+			const items = await response.json()
+			if (!ignore)
+				setItems(items)
+		}
+
+		getItems()
+
+		return () => {
+			ignore = true
+		}
+	}, [])
+
+	return (
+		<>
+			<h1>Best Before</h1>
+			<Inventory items={items} />
+		</>
+	)
 }
